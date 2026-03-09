9dd782908b06a39aaeb9c1009ceaad525e172223
covotore <cvt@move-effect.net>
2026-03-09 09:53:55 +0900

    Update service worker cache and add new icons

--- sw.js
+++ sw.js
@@ -1,42 +1,66 @@
-const CACHE_NAME = "tarot-pwa-v1";
+const CACHE_NAME = "tarot-pwa-v2";
 
 const urlsToCache = [
 
-"/",
-"/index.html",
-"/style.css",
-"/script.js",
-"/manifest.json",
-"/images/back.png"
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json",
+    "./icons/icon-192.png",
+    "./icons/icon-512.png",
+    "./images/back.png",
+    "./images/chariot.png",
+    "./images/death.png",
+    "./images/devil.png",
+    "./images/emperor.png",
+    "./images/empress.png",
+    "./images/fool.png",
+    "./images/hangedman.png",
+    "./images/hermit.png",
+    "./images/hierophant.png",
+    "./images/judgement.png",
+    "./images/justice.png",
+    "./images/lovers.png",
+    "./images/magician.png",
+    "./images/moon.png",
+    "./images/priestess.png",
+    "./images/star.png",
+    "./images/strength.png",
+    "./images/sun.png",
+    "./images/temperance.png",
+    "./images/tower.png",
+    "./images/wheel.png",
+    "./images/world.png"
 
 ];
 
 self.addEventListener("install", event => {
 
-event.waitUntil(
+    event.waitUntil(
 
-caches.open(CACHE_NAME)
-.then(cache => {
+        caches.open(CACHE_NAME)
+            .then(cache => {
 
-return cache.addAll(urlsToCache)
+                return cache.addAll(urlsToCache)
 
-})
+            })
 
-)
+    )
 
 })
 
 self.addEventListener("fetch", event => {
 
-event.respondWith(
+    event.respondWith(
 
-caches.match(event.request)
-.then(response => {
+        caches.match(event.request)
+            .then(response => {
 
-return response || fetch(event.request)
+                return response || fetch(event.request)
 
-})
+            })
 
-)
+    )
 
 })
